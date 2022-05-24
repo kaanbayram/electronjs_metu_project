aaf5b271942bdf05277534c87bf124b18086cd81
kaanbayram <kaan.9898@hotmail.com>
2022-05-24 04:13:15 +0300

    [feature] - Creating Project

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    height: 500,
+    width: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "logics.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
